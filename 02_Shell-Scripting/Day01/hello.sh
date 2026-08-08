87525bc519e8aa961ea5fb211ab9796aa80fc0f4
ankittripathidevs <ankittripathi2k24@gmail.com>
2026-08-08 09:07:18 +0000

    day-01

--- 02_Shell-Scripting/Day01/hello.sh
+++ 02_Shell-Scripting/Day01/hello.sh
@@ -2,6 +2,12 @@
 
 # Day-01 of Learning shell scripting
 
+<< comments 
+(1) To execute this file, make the file executable by changing file permission uing chmod 755
+(2) To run this file type bash fileName or ./fileName
+comments
+
+
 echo "Platform: YouTube"
 
 echo "Instructor: TrainWithSubham"

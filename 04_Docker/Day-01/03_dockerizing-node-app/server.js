c99ab369943779f863d1098a9baa55a74aec414e
ankittripathidevs <ankittripathi2k24@gmail.com>
2026-08-20 20:59:37 +0000

    Day-01: dockerize node app

---
+++ 04_Docker/Day-01/03_dockerizing-node-app/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Dosto!");
+});
+
+app.get("/greet", (req, res)=> {
+  res.send("Hello, Namaste")
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
